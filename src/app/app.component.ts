cf33026e1791e3d457584321381b30ed40fde9ae
Anderson Almeyda <forastero0225@gmail.com>
2022-09-12 19:58:11 -0500

    masna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Piano-App';
+
+  aplicarSonido(numero:number): void {
+    const audio = new Audio(`../assets/sonidos/note${numero}.wav`);
+    audio.load();
+    audio.play();
+  }
 }
